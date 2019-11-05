268bef8fa4d909a3f81d960c0467620a834238da
oZoon <13639082+oZoon@users.noreply.github.com>
2019-11-05 23:54:08 +0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,3 +1,6 @@
+define ('D', DIRECTORY_SEPARATOR);
+define ('INIT_URI', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].D);
+
 $a = array();
 $a['allowSymbols'] = '1234567890ghijklmnopqrstuvwxyzGHIJKLMNOPQRSTUVWXYZ';
 $a['mysql'] = new mysqli('localhost', 'aaa', 'aaa', 'aaa');

176cf5fe30e956353081e99e5f48ab11de68944c
oZoon <13639082+oZoon@users.noreply.github.com>
2019-11-06 00:07:20 +0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -4,3 +4,6 @@ define ('INIT_URI', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].D);
 $a = array();
 $a['allowSymbols'] = '1234567890ghijklmnopqrstuvwxyzGHIJKLMNOPQRSTUVWXYZ';
 $a['mysql'] = new mysqli('localhost', 'aaa', 'aaa', 'aaa');
+
+include_once 'appExt.php';
+include_once 'appBase.php';

afd44fa3c8a11d78bce5f65b7efb64e1aa130f0e
oZoon <13639082+oZoon@users.noreply.github.com>
2019-11-06 10:41:37 +0500

    Delete start.php

--- start.php
+++
@@ -1,4 +0,0 @@
-include_once 'settings.php';
-
-// all of $var1...$var3 places into $a['qsArr'] array, like this $a['qsArr']['var1']
-echo '<a href="/'.e('var1=1&var2=2&var3=3', 'y').'">test &lt;a&gt;</a>';
